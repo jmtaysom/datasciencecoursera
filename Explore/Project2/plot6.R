1c441133368d2dbf623301b1f0f5de93620c4755
jmtaysom <james.taysom@gmail.com>
2015-03-17 16:53:19 -0400

    Adding last files to project

--- Explore/Project2/plot6.R
+++ Explore/Project2/plot6.R
@@ -25,20 +25,23 @@ rm(list=ls())
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## summarise the data to find the total emissions (in millions of tons) per year
-# Baltimore <- NEI[NEI$fips=='24510',]
+## summarise the data to find the total emissions (in tons) per year
 vehiclesSCC <- SCC[grepl("Vehicle", SCC$SCC.Level.Two), ]
 vehicles <- NEI[NEI$SCC %in% vehiclesSCC$SCC,] 
-Baltimore_vehicles <- vehicles[vehicles$fips == '24510',]
-totals <- ddply(Baltimore_vehicles,.(year), summarise, Emissions=sum(Emissions))
-#totals$Emissions <- totals$Emissions/1000
+compare_vehicles <- vehicles[vehicles$fips == '24510' | vehicles$fips == '06037',]
+totals <- ddply(compare_vehicles,.(year, fips), summarise, Emissions=sum(Emissions))
+## change the fips codes to City Names
+totals$fips[totals$fips=='24510'] <- 'Baltimore'
+totals$fips[totals$fips=='06037'] <- 'Los Angeles'
+colnames(totals)[which(names(totals) == 'fips')] <- 'City'
 
 ## Plot the emissions over time and add a linear regression line
-png(file="plot5.png",width=480,height=480)
-g <- ggplot(totals, aes(year, Emissions)) 
+png(file="plot6.png",width=480,height=480)
+g <- ggplot(totals, aes(year, Emissions, color=City)) 
 g + geom_point() + geom_smooth(method='lm', se=FALSE) + 
-  labs(title='PM 2.5 Emissions from Vehicles in Baltimore',
+  labs(title='PM 2.5 Emissions from Vehicles',
        y='Emission in tons', x="Year") +
-  scale_x_continuous(breaks=c(1999,2002,2005,2008))
+  scale_x_continuous(breaks=c(1999,2002,2005,2008)) +
+  theme(legend.justification=c(1,0), legend.position=c(1,0.4))
 
 dev.off()
