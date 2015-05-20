1d06ebf6cebabdecf9b5602e0266f1a828cd63a0
jmtaysom <james.taysom@gmail.com>
2015-05-19 20:22:20 -0400

    Plotted uniform distrobution

--- stats/Course_Project.R
+++ stats/Course_Project.R
@@ -1,7 +1,15 @@
+library(ggplot2)
+
+
 set.seed(0)
+
+uni <- runif(1000)
+uni.df <- data.frame(uni)
+u <- ggplot(uni.df,aes(x=uni))
+u + geom_histogram(aes(fill = ..count..))
+
 mns = NULL
 for (i in 1 : 1000) mns = c(mns, mean(runif(40)))
-
 df <- data.frame(mns)
 m <- ggplot(df, aes(x=mns))
 m + geom_histogram(aes(fill = ..count..))
