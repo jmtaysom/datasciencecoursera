917488060530db8828c3ed5088d33d0844f19709
jmtaysom <james.taysom@gmail.com>
2015-05-19 20:33:07 -0400

    plotted distrobution of exponential averages

--- stats/Course_Project.R
+++ stats/Course_Project.R
@@ -13,3 +13,9 @@ for (i in 1 : 1000) mns = c(mns, mean(runif(40)))
 df <- data.frame(mns)
 m <- ggplot(df, aes(x=mns))
 m + geom_histogram(aes(fill = ..count..))
+
+expn = NULL
+for (i in 1:1000) expn = c(expn, mean(rexp(40,.2)))
+xdf <- data.frame(expn)
+x <- ggplot(xdf,aes(x=expn))
+x + geom_histogram(aes(fill = ..count..))
