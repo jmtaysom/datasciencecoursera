698821290910db17a7a9464e27163572ad362bf6
jmtaysom <james.taysom@gmail.com>
2015-05-23 22:09:59 -0400

    Finished part 1

--- stats/Course_Project.R
+++ stats/Course_Project.R
@@ -19,6 +19,10 @@ expn = NULL
 for (i in 1:1000) expn = c(expn, mean(rexp(40,.2)))
 xdf <- data.frame(expn)
 x <- ggplot(xdf,aes(x=expn)) + geom_histogram(aes(fill = ..count..), binwidth=.25) + xlim(2,8) +ylim(0,150)
+abline(v=5) 
+
+x2 <- ggplot(xdf,aes(x=expn))
+x2 + geom_histogram(aes(fill = ..count..)) + geom_vline(xintercept = mean(expn), color='red') + geom_vline(xintercept = 1/lambda, color='black')+n
 
 mean(expn)
 1/.2
@@ -28,8 +32,24 @@ sd(expn)
 
 
 
-norm <-rnorm(1000,mean=5, sd = 5/sqrt(40))
+norm <-rnorm(1000,mean=5, sd = 5)
 ndf <- data.frame(norm)
-n <- ggplot(ndf, aes(x=norm)) + geom_histogram(aes(fill = ..count..), binwidth=.25) + xlim(2,8) +ylim(0,150)
+n <- ggplot(ndf, aes(x=norm)) + geom_density(color='blue')
+n +x2
+
+ndf$distribution <- 'population'
+names(ndf)[names(ndf)=="norm"] <- "value"
+xdf$distribution <- 'sample'
+names(xdf)[names(xdf)=="expn"] <- "value"
+combined <- rbind(xdf,ndf)
+
+ggplot(combined, aes(x=value, colour=distribution)) + geom_density()
 
 grid.arrange(x,n, ncol=2, main='Exponential distribution vs. normal distribution')
+
+
+
+
+# Project part 2
+
+
